33509632c9a1740bab273102c0d80132fe8104fd
dmitrikivilev <dimon_95.08@mail.ru>
2025-11-15 22:40:38 +0300

    Add interactive interface

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,20 @@
+import prompt
+
+
+def welcome():
+    print("Первая попытка запустить проект!")
+    print("***")
+    print("<command> exit - выйти из программы")
+    print("<command> help - справочная информация")
+    
+    while True:
+        command = prompt.string("Введите команду: ")
+        
+        if command == "exit":
+            print("Выход из программы. До свидания!")
+            break
+        elif command == "help":
+            print("<command> exit - выйти из программы")
+            print("<command> help - справочная информация")
+        else:
+            print(f"Неизвестная команда: {command}")
